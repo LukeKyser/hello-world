4faed320605ec57284c2c86faee75bce10073ecc
LukeKyser <30376877+LukeKyser@users.noreply.github.com>
2017-07-22 14:14:38 -0700

    Add HelloWorld.java file for first pull request

---
+++ HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * This class is to act as a submission to GitHub in order to become more familiar with the VCS
+ *
+ * @author Luke Kyser
+ * @version 2014.07.22
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hey, Luke Kyser here..");
+    }
+}
